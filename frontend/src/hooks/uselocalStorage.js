df0997b974082e90c7d058397ee05c2557bd4b4b
hristo.grudev <hristo.grudev@adata.pro>
2023-12-05 10:05:28 +0200

    Delete auth token after expire from localStorage

--- frontend/src/hooks/uselocalStorage.js
+++ frontend/src/hooks/uselocalStorage.js
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useLocalStorage = (key, defaultValue) => {
     const [value, setValue] = useState(() => {
         const storedData = localStorage.getItem(key);
+
 
         return storedData ? JSON.parse(storedData) : defaultValue;
     });
 
     const setLocalStorageValue = (newValue) => {
         localStorage.setItem(key, JSON.stringify(newValue));
-
         setValue(newValue);
     };
+
+    useEffect(() => {
+        const storedData = localStorage.getItem(key);
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
+            const storedTimestamp = parsedData.timestamp;
+            const currentTimestamp = Date.now();
+            const timeDifference = (currentTimestamp - storedTimestamp) / (1000 * 60);
+
+            if (timeDifference >= 60) {
+                setValue({});
+                localStorage.removeItem(key);
+            }
+        }
+    }, [key]);
 
     return [
         value,
