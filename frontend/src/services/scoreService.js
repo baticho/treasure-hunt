aa9132315571be2aecc74caa98b65824a5d909d9
hristo.grudev <hristo.grudev@adata.pro>
2023-12-07 10:01:01 +0200

    Fix scoore url

--- frontend/src/services/scoreService.js
+++ frontend/src/services/scoreService.js
@@ -1,5 +1,5 @@
 import * as request from "./requester";
 
-const baseUrl = `${import.meta.env.VITE_API_URL}/scores/`;
+const baseUrl = `${import.meta.env.VITE_API_URL || 'api'}/scores/`;
 
 export const newScore = (scoreData) => request.post(baseUrl, scoreData);
