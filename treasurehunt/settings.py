7f9ab9b968c50deb19088cdcc599d2cd91735bfb
hristo.grudev <hristo.grudev@adata.pro>
2023-12-07 12:01:57 +0200

    Enable CSRF

--- treasurehunt/settings.py
+++ treasurehunt/settings.py
@@ -180,7 +180,3 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
 }
-
-CSRF_COOKIE_SECURE = False
-CSRF_USE_SESSIONS = False
-
