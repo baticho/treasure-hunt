1781ab5fe780e09ee7654baf87797f25344601f6
hristo.grudev <hristo.grudev@adata.pro>
2023-12-08 10:52:03 +0200

    Fix api url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,19 +3,19 @@ import * as request from "./requester";
 let baseUrl;
 
 if (import.meta.env.VITE_API_URL) {
-  baseUrl = `${import.meta.env.VITE_API_URL}/accounts`;
+  baseUrl = `${import.meta.env.VITE_API_URL}/accounts/`;
 } else {
   const domain = window.location.origin;
-  baseUrl = `${domain}/api/accounts`;
+  baseUrl = `${domain}/api/accounts/`;
 }
 
 export const login = (username, password) => 
-    request.post(`${baseUrl}/login/`, { username, password });
+    request.post(`${baseUrl}login/`, { username, password });
 
 
 export const logout = async (refresh) => {
     try {
-        const response = await fetch(`${baseUrl}/logout/`, {
+        const response = await fetch(`${baseUrl}logout/`, {
             method: 'POST',
             headers: {
                 'Content-Type': `application/json`
@@ -32,4 +32,4 @@ export const logout = async (refresh) => {
 };
 
 export const register = (username, email, first_name, last_name, password, password2) =>
-    request.post(`${baseUrl}/register/`, {username, email, first_name, last_name, password, password2});
+    request.post(`${baseUrl}register/`, {username, email, first_name, last_name, password, password2});
--- frontend/src/services/treasureHuntService.js
+++ frontend/src/services/treasureHuntService.js
@@ -3,10 +3,10 @@ import * as request from "./requester";
 let baseUrl;
 
 if (import.meta.env.VITE_API_URL) {
-  baseUrl = `${import.meta.env.VITE_API_URL}/treasure-hunts`;
+  baseUrl = `${import.meta.env.VITE_API_URL}/treasure-hunts/`;
 } else {
   const domain = window.location.origin;
-  baseUrl = `${domain}/api/treasure-hunts`;
+  baseUrl = `${domain}/api/treasure-hunts/`;
 }
 
 export const getAll = () => request.get(baseUrl);
